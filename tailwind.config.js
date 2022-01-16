c70f3f1924e7f8eea4645deb4b169abffeee933e
Fadziso Gabrielle <fadziso@novodigital.biz>
2022-01-16 16:51:31 +0200

    Modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 module.exports = {
+  darkMode: 'class',
   content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      backgroundImage: {
+        'hero': "url('./src/images/hero.jpg')",
+      }
+    },
   },
   plugins: [],
 }
